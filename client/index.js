14851416446e8c5a79354a44d21caf8fe6b4db95
Mark Lee <marklee22@gmail.com>
2013-05-04 13:05:20 -0700

    Implemented basic instagram and Pearson kitchen API calls

--- client/index.js
+++ client/index.js
@@ -7,5 +7,19 @@ Template.hello.events({
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
       console.log("You pressed the button");
+  },
+
+  'click #getRecipes': function() {
+    console.log('test complete!');
+    Meteor.call('foodQuery', function(err, results) {
+      console.log(results);
+    });
+  },
+
+  'click #getInstagramPhotos': function() {
+    console.log('get Instagram Photos');
+    Meteor.call('getInstagramFoodPics', function(err, results) {
+      console.log(results);
+    });
   }
 });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
-Meteor.startup(function () {
-  // code to run on server at startup
+// Pearson API Settings
+var pearsonAPIKey = 'aac3c31fec0797148cb1a68b469282ce';
+var pearsonURL = 'https://api.pearson.com/kitchen-manager/v1/';
+
+// Instagram API Settings
+var instagramURL = 'https://api.instagram.com/v1/';
+var instagramClientID = '9da20f6b166e474a8b70cb396aa87da7';
+var instagramSecret = '587de29c36b4471d8df52e2fe61afc4e';
+
+Meteor.methods({
+  'foodQuery': function() {
+    console.log('Running food query');
+    var url = pearsonURL + 'recipes.json?offset=2&limit=2&apikey=' + pearsonAPIKey;
+    var results = Meteor.http.get(url);
+    console.log(results);
+    return results;
+  },
+
+  'getInstagramFoodPics': function(tagName) {
+    console.log('Running Instagram query');
+    var url = instagramURL + 'media/popular?client_id=' + instagramClientID;
+    var results = Meteor.http.get(url);
+    console.log(results);
+    return results;
+  }
 });
