58b49ab4830b07af7b6bfb357cec9276d6ffa6d6
Mark Lee <marklee22@gmail.com>
2013-05-04 15:15:20 -0700

    Added yummly API calls

--- client/index.js
+++ client/index.js
@@ -2,6 +2,14 @@ Template.hello.greeting = function () {
   return "Welcome to RecipeGram.";
 };
 
+Template.food_photos.photos = function() {
+  return Session.get('photos').images;
+};
+
+Template.recipes.recipes = function() {
+  return Session.get('recipes').data.matches;
+};
+
 Template.hello.events({
   'click input' : function () {
     // template data, if any, is available in 'this'
@@ -11,15 +19,25 @@ Template.hello.events({
 
   'click #getRecipes': function() {
     console.log('test complete!');
-    Meteor.call('foodQuery', function(err, results) {
+    Meteor.call('getPearsonRecipes', 'breakfast', function(err, results) {
       console.log(results);
+      Session.set('recipes', results);
+    });
+  },
+
+  'click #getYummlyRecipes': function() {
+    console.log('yummly');
+    Meteor.call('getYummlyRecipes', function(err, results) {
+      console.log(results);
+      Session.set('recipes', results);
     });
   },
 
   'click #getInstagramPhotos': function() {
     console.log('get Instagram Photos');
-    Meteor.call('getInstagramFoodPics', function(err, results) {
+    Meteor.call('getInstagramFoodPics', 'chickenTeriyaki', function(err, results) {
       console.log(results);
+      Session.set('photos', results);
     });
   }
 });
