0f854c5d108b99a6a9268126457907e8e6b542e8
Philip Rosen <philipjrosen@Philip-Rosens-MacBook-Air.local>
2013-05-04 13:04:50 -0700

    basic button functionality working

--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
-Template.hello.greeting = function () {
-  return "Welcome to RecipeGram.";
-};
+// Template.hello.greeting = function () {
+//   return "Welcome to RecipeGram.";
+// };
 
-Template.hello.events({
-  'click input' : function () {
+Template.meals.events({
+  'click input#breakfast' : function () {
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
-      console.log("You pressed the button");
+      console.log("You pressed the breakfast button");
+  },
+  'click input#lunch' : function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the lunch button");
+  },
+  'click input#dinner' : function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the dinner button");
   }
 });
