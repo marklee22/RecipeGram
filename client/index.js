02b0c164c25bddf32b439effff1309f53d463acd
Philip Rosen <philipjrosen@Philip-Rosens-MacBook-Air.local>
2013-05-04 14:28:14 -0700

    routing functionality working for one button

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
 // Template.hello.greeting = function () {
 //   return "Welcome to RecipeGram.";
 // };
+Ingredients = new Meteor.Collection('ingredients');
 
 Template.meals.events({
   'click input#breakfast' : function () {
+  	Session.set('show_breakfast',true);
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
       console.log("You pressed the breakfast button");
@@ -19,3 +21,15 @@ Template.meals.events({
       console.log("You pressed the dinner button");
   }
 });
+// Template.ingredients.ingredients = function(){
+//     return Ingredients.find({}, { sort: {time: 1} });
+// };
+
+Session.set('show_breakfast', false);
+
+Template.router.breakfast = function () { 
+	return Session.equals('show_breakfast',true);
+};
+
+
+
